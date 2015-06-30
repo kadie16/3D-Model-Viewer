5d9e1fc692c7f01da02e7e84d97dfdbfe0e9b8ae
Kadie16 <kadie@berkeley.edu>
2015-06-30 14:51:03 +0800

    working on loading file

--- projectV1/objload.cpp
+++ projectV1/objload.cpp
@@ -5,6 +5,20 @@ objLoad::objLoad(string fName)
 {
     cout << "Loading object from file:  " << fName << endl;
     fileName = fName;
+    /* open file stream */
+    ifstream stream(fName);
+    /* determine length of file */
+    is.seekg(0, ios_base::end);
+    size_t size = is.tellg();
+    is.seekg(0, ios_base::begin);
+    /* fill vector */
+    string currLine;
+    while (stream.peek() !=EOF) {
+        getline(stream, currLine);
+    }
+
+
+
 }
 
 string objLoad::getFileName()
