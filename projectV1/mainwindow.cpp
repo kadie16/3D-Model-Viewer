0f957ee24cba09d7608ce3db8dccc34c04586248
Kadie16 <kadie@berkeley.edu>
2015-07-06 15:48:45 +0700

    added fileDialog prompt to mainWindow

--- projectV1/mainwindow.cpp
+++ projectV1/mainwindow.cpp
@@ -1,7 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-
+#include <QFileDialog>
+#include <QMessageBox>
+#include "objload.h"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -12,4 +13,29 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(
+                this,
+                tr("Open File"),
+                "C://",
+                "Object File (*.obj)"
+                );
+     objLoad loader(fileName);
+     std::vector<Vertex> vertices = loader.getVertices();
+     glClear(GL_COLOR_BUFFER_BIT);
+     glRotatef(0.5,1,1,1);
+     Vertex v;
+     /* load coordinates into triangles through loop */
+     /* refer to boxes for examples on interaction */
+     glBegin(GL_TRIANGLES);
+
+     for (unsigned i = 0 ; i < vertices.size() ; i++)
+     {
+         v = vertices.at(i);
+         glVertex3f(v.getX(), v.getY(), v.getZ());
+      }
+     glEnd();
 }
